eebebe6be72c7e24c928646693ee8131f668e6b5
Dundee Zhang <60833894+dundeezhang@users.noreply.github.com>
2026-01-30 17:17:31 -0800

    Update AttentionApp to version 1.0.1

--- Casks/attentionapp.rb
+++ Casks/attentionapp.rb
@@ -1,6 +1,6 @@
 cask "attentionapp" do
-  version "1.0.0"
-  sha256 "0f24f0cd09088c2d5da159baacb9254df253b63d2e10e16e3bfe5797c60b6968"
+  version "1.0.1"
+  sha256 "343d616040a8b0a25b7a64dc4178639021a023591dd10a33099c9cc472892d03"
 
   url "https://github.com/dundeezhang/attention-fix/releases/download/v#{version}/AttentionApp-v#{version}.zip"
   name "AttentionApp"
